e960f7c99a6fcb90c2c0f7ecaaf69f485bf9410b
ctarrington <arrington.ct@gmail.com>
2019-06-24 22:51:55 -0400

    unit tests for random

--- project/src/lib/util/random.ts
+++ project/src/lib/util/random.ts
@@ -1,11 +1,18 @@
+/**
+ * returns a number in the half open interval [min, max)
+ * @param min
+ * @param max
+ */
 export const randomBetween = (min: number, max: number): number => {
   return min + (max - min) * Math.random();
 };
 
 /**
- * returns an integer in the closed set [min, max]
+ * returns an integer in the closed set [min, min+1, ..., max]
+ * @param min
+ * @param max
  */
 export const randomInt = (min: number, max: number): number => {
-  const decimal = randomBetween(Math.floor(min), Math.floor(max) + 0.99999999);
-  return Math.floor(decimal);
+  const decimal = randomBetween(min - 0.5, max + 0.5);
+  return Math.round(decimal);
 };
