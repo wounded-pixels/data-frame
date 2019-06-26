733505d55a9d3a8fe5c0cd3e07348270965921e6
ctarrington <arrington.ct@gmail.com>
2019-06-25 21:15:12 -0400

    more complete unit tests for random

--- project/src/lib/util/random.test.ts
+++ project/src/lib/util/random.test.ts
@@ -13,13 +13,14 @@ test('random integers', () => {
     }
   }
 
+  expect(Object.keys(buckets).length).toBe(3);
   expect(buckets[5] / 10000).toBeCloseTo(SAMPLE_SIZE / 30000, 0);
   expect(buckets[6] / 10000).toBeCloseTo(SAMPLE_SIZE / 30000, 0);
   expect(buckets[7] / 10000).toBeCloseTo(SAMPLE_SIZE / 30000, 0);
 });
 
-function validateBetween(low: number, high: number, scale = 1) {
-  const SAMPLE_SIZE = 1e4;
+function validateBetween(low: number, high: number) {
+  const SAMPLE_SIZE = 1e6;
 
   const values = [];
   for (let ctr = 0; ctr < SAMPLE_SIZE; ctr++) {
@@ -31,13 +32,21 @@ function validateBetween(low: number, high: number, scale = 1) {
   }, 0);
 
   const mean = sum / SAMPLE_SIZE;
-  const min = Math.min(...values);
-  const max = Math.max(...values);
+  const min = values.reduce(
+    (previous, current) => Math.min(previous, current),
+    Number.MAX_VALUE
+  );
+  const max = values.reduce(
+    (previous, current) => Math.max(previous, current),
+    Number.MIN_VALUE
+  );
   const expectedMean = low + (high - low) / 2;
 
-  expect(mean / scale).toBeCloseTo(expectedMean / scale, 0);
-  expect(min).toBeCloseTo(low, 0);
-  expect(max).toBeCloseTo(high, 0);
+  expect(mean).toBeCloseTo(expectedMean, 0);
+  expect(min).toBeCloseTo(low, 2);
+  expect(max).toBeCloseTo(high, 2);
+  expect(min).toBeGreaterThanOrEqual(low);
+  expect(max).toBeLessThanOrEqual(high);
 }
 
 test('0.555 to 0.556', () => {
@@ -49,5 +58,5 @@ test('-10 to 10', () => {
 });
 
 test('-5 to 500', () => {
-  validateBetween(-5, 500, 10);
+  validateBetween(-5, 500);
 });
